ffed6c6f31eac63e2f38a77a56a8d19b8e220ea7
Presidente-Fornoni <154509433+Presidente-Fornoni@users.noreply.github.com>
2024-11-12 11:38:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function fetchData(){
+
+     try{
+    const nomepokemon = document.getElementById("pknome").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomepokemon}`);
+    
+    if(!response.ok){
+        throw new Error ("Impossibile trovare il pokemon. Verificare che il nome sia stato inserito correttamente");
+    }
+
+    const data = await response.json();
+    const pkSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokeSprite");
+
+    imgElement.src = pkSprite;
+    imgElement.style.display= "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+
+}
